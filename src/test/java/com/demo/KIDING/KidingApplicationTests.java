5670cbd1ef1fd1b7f06289dbb37619a0048176d6
ChaeyeonHan <kate2236e@naver.com>
2023-11-14 20:37:17 +0900

    initial commit

---
+++ src/test/java/com/demo/KIDING/KidingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.KIDING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
